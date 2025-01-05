9b34380a76bb80d72dde9fe964f2469affa61369
Vaibhav2321 <vaibhavverma2321@gmail.com>
2025-01-05 21:28:11 +0530

    handle cors error

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import messageRoutes from './route/message.route.js'
 import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
